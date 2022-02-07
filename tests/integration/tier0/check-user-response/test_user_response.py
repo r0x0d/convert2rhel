10f39cfa1f394ba711bbcbb6f1e346b9228b7e5f
danmyway <ddiblik@redhat.com>
2022-02-07 18:20:21 +0100

    add test for organization prompt

--- tests/integration/tier0/check-user-response/test_user_response.py
+++ tests/integration/tier0/check-user-response/test_user_response.py
@@ -13,15 +13,27 @@ def test_check_user_response(convert2rhel):
         c2r.expect(" ... activation key not found, username and password required")
         c2r.expect("Username: ")
         c2r.sendline()
-        assert c2r.expect("Username: ") == 0
+        c2r.expect("Username: ")
         # Provide username, expect password prompt
         c2r.sendline(env.str("RHSM_USERNAME"))
         c2r.expect("Password: ")
         c2r.sendline()
-        assert c2r.expect("Password: ") == 0
+        c2r.expect("Password: ")
         # Provide password, expect registration
         c2r.sendline(env.str("RHSM_PASSWORD"))
-        assert c2r.expect("Registering the system using subscription-manager ...") == 0
-        assert c2r.expect("Enter number of the chosen subscription: ") == 0
+        c2r.expect("Registering the system using subscription-manager ...")
+        c2r.expect("Enter number of the chosen subscription: ")
+        c2r.sendcontrol("d")
+    assert c2r.exitstatus != 0
+
+    with convert2rhel(
+        "-y --no-rpm-va --serverurl {} -k {}".format(env.str("RHSM_SERVER_URL"), env.str("RHSM_KEY"))
+    ) as c2r:
+        c2r.expect("Checking for activation key ...")
+        c2r.expect("Organization: ")
+        c2r.sendline()
+        c2r.expect("Organization: ")
+        c2r.sendline(env.str("RHSM_ORG"))
+        c2r.expect("Registering the system using subscription-manager ...")
         c2r.sendcontrol("d")
     assert c2r.exitstatus != 0
